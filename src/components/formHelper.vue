167411cd3ef90437608da1b4bc7a4d5bd161b7a0
Antonio Erdeljac <hedhogs@gmail.com>
2020-10-05 00:47:01 +0200

    slots practice

---
+++ src/components/formHelper.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <slot name="title"></slot>
+    <h1>Form Helper</h1>
+    <slot name="text"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+  h1 {
+    color: red;
+  }
+</style>
